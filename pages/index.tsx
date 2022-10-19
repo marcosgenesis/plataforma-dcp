9ca476822fcc4440e2b05571609d54607d23b7db
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2022-10-19 14:59:06 -0300

    chore: esboço waitlist

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,53 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const router = useRouter();
   return (
-    <div>opa</div>
-  )
-}
+    <div style={{ width: "100%"}}>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          marginTop: "24px",
+          height: "60px",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+          <img src='https://media.tenor.com/YxQRjVsG73cAAAAC/caneta-azul.gif' style={{ width: "100px", height: "100px"}}></img>
+      
+        <button
+          style={{
+            border: "none",
+            padding: "1rem 3rem",
+            borderRadius: "8px",
+            fontWeight: "600",
+            cursor: "pointer",
+            textDecoration: "none",
+          }}
+          onClick={() => router.push("waitlist")}
+        >
+          Garantir acesso
+        </button>
+      </div>
+      <div
+        style={{
+          background: "#eee",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "5rem",
+          height: "10vh",
+        }}
+      >
+        Restante da landing page
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
