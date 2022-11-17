a1b4112196c64ba1ec516ac8c3a1f998fff09489
Jeferson Gonçalves <jeferson.goncalves.noronha@gmail.com>
2022-11-17 17:44:53 -0300

    feat: creating the steps and plans

--- src/pages/signature.tsx
+++ src/pages/signature.tsx
@@ -2,44 +2,99 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import { FilledButton } from "../components/buttons/FilledButton"
 import ChekcoutItem from "../components/checkoutItem/ItemContent";
 import CheckoutItemFooter from '../components/checkoutItem/ItemFooter'
 
 import { Container, ContainerPlans, Checkout, ContainerPrice, ContainerSignature, 
-Cupom, PlansItens, Steps, CheckoutHeader, CheckoutContent, CheckoutFooter, 
-ButtonCode } 
+Cupom, PlansItens, StepsContainer, CheckoutHeader, CheckoutContent, CheckoutFooter, 
+ButtonCode, Step, Title, Plan, DivisionVertical} 
 from "../styles/signature";
+import { useState } from "react";
 
 const Signature: NextPage = () => {
+  const [step, setStep] = useState(1)
+
+  const nextStep = () => {
+    setStep(step + 1)
+  }
+
+  const backStep = () => {
+    setStep(step - 1)
+  }
+
   return(
     <Container>
       <Navbar />
       <ContainerSignature>
         <ContainerPlans>
-          <Steps>
-            <div>
-              <button> 1 </button>
+          <StepsContainer>
+            <Step>
+              <div> 1 </div>
               <span> Plano </span>
-            </div>
+              <div className="division"/>
+            </Step>
 
-            <div>
-              <button> 2 </button>
+            <Step>
+              <div> 2 </div>
               <span> Identificação </span>
-            </div>
+              <div className="division"/>
+            </Step>
 
-            <div>
-              <button> 3 </button>
+            <Step>
+              <div> 3 </div>
               <span> Endereço </span>
-            </div>
+              <div className="division"/>
+            </Step>
 
-            <div>
-              <button> 4 </button>
+            <Step>
+              <div> 4 </div>
               <span> Pagamento </span>
-            </div>
-          </Steps>
-          <PlansItens>
+            </Step>
+          </StepsContainer>
+          {
+            step === 1 && (
+            <>
+              <Title>Escolha seu plano de assinatura</Title>
+              <Plan htmlFor="radio-1">
+                <input type="radio" id="radio-1" name="opcoes"/>
+                <DivisionVertical />
+                <div>
+                  <p>Lançamento: Coleção completa (oferta limitada) </p>
+                  <ul>
+                    <li> Receba todas as caixas de uma só vez! </li>
+                    <li> Parcele em até 8x sem juros no cartão de crédito. </li>
+                  </ul>
+                </div>
+                <span> R$ 600,00 </span>
+              </Plan>
 
-          </PlansItens>
+              <Plan htmlFor="radio-2" className="plans">
+                <input type="radio" id="radio-2" name="opcoes"/>
+                <DivisionVertical />
+                <div>
+                  <p>Plano Anual </p>
+                  <ul>
+                    <li> Parcele em até 12x sem juros no cartão de crédito. </li>
+                    <li> Economize R$ 127,00. </li>
+                  </ul>
+                </div>
+                <span> R$ 1.500,00 </span>
+              
+              </Plan>
+            </>
+            )
+          }
+
+          {
+            step === 2 && (
+              <p>é o segundo</p>
+            )
+          }
+          
+
+        <FilledButton width="110px" onClick={nextStep}> Próximo  </FilledButton>
+          
         </ContainerPlans>
         
         <ContainerPrice>
--- src/styles/signature.ts
+++ src/styles/signature.ts
@@ -20,13 +20,167 @@ export const ContainerPlans = styled.div`
 
   border: 1px solid rgba(0, 0, 0, 0.06);
   border-radius: 12px;
+
+  button {
+    align-self: flex-end;
+  }
 `;
 
-export const Steps = styled.div`
+export const StepsContainer = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-export const PlansItens = styled.div``;
+export const Step = styled.div`
+  display: flex;
+  align-items: center;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 28px;
+    height: 28px;
+
+    background: #0055ff;
+    border-radius: 40px;
+
+    font-family: "DM Sans";
+
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 20px;
+
+    color: #ffffff;
+  }
+
+  span {
+    margin-left: 8px;
+    font-family: "DM Sans";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 20px;
+
+    color: #0055ff;
+
+    & + div.division {
+      width: 24px;
+      height: 0px;
+
+      margin-left: 24px;
+      border: 2px solid #e6e6e6;
+    }
+  }
+`;
+
+export const Title = styled.p`
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 32px;
+  margin: 5px 0 0 0;
+  color: #333333;
+`;
+
+export const Plan = styled.label`
+  position: relative;
+  box-sizing: border-box;
+
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+
+  padding: 24px;
+
+  width: 100%;
+  height: 112px;
+
+  background: #ffffff;
+
+  border: 1px solid #cccccc;
+  border-radius: 20px;
+  cursor: pointer;
+
+  input {
+    display: none;
+  }
+
+  input:checked + div,
+  input:checked + div + div p,
+  input:checked + div + div + span {
+    color: #0055ff;
+    border-color: #0055ff;
+  }
+
+  &::before {
+    box-sizing: border-box;
+    content: "";
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    background: #ffffff;
+    border: 3px solid rgba(0, 0, 0, 0.1);
+  }
+
+  &:has(input:checked) {
+    background: #e6eeff;
+    border-color: #e6eeff;
+
+    ::before {
+      text-align: center;
+      justify-self: center;
+      content: "\u2714";
+      color: #ffffff;
+      background: #0055ff;
+      border: none;
+    }
+  }
+
+  p,
+  span {
+    font-family: "DM Sans";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    margin-bottom: 5px;
+    color: #000000;
+  }
+
+  span {
+    position: absolute;
+    right: 0;
+    top: 0;
+    padding: 24px;
+  }
+
+  ul {
+    height: 40px;
+
+    li {
+      font-family: "DM Sans";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 20px;
+
+      color: rgba(0, 0, 0, 0.66);
+    }
+  }
+`;
+
+export const DivisionVertical = styled.div`
+  width: 0px;
+  height: 100%;
+  align-items: center;
+  margin: 24px;
+
+  border: 1px solid #b3b3b3;
+`;
 
 export const ContainerPrice = styled.div`
   display: flex;
