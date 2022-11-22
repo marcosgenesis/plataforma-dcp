5d698ae2a81bf9933561eec0a7de3df6d1df34b7
Jeferson Gonçalves <jeferson.goncalves.noronha@gmail.com>
2022-11-22 20:23:20 -0300

    feat: Creating title component

---
+++ src/components/title/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const TitleContainer = styled.h2`
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 32px;
+  margin: 5px 0 0 0;
+  color: #333333;
+`;
