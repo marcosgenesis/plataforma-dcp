5b4175d6033af7cf07189d441991d3798a840d12
Jeferson Gonçalves <jeferson.goncalves.noronha@gmail.com>
2022-11-23 00:27:46 -0300

    feat: creating form component for the plan

---
+++ src/components/stepsForm/planForm/index.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { FieldValues, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import schema from './validation'
+
+import Plan from "../../plan";
+import Title from "../../title";
+import { FilledButton } from "../../buttons/FilledButton"
+
+import {ContainerForm, MessageErro, Buttons} from './style'
+
+
+interface PlanProps {
+  nextStep: () => void
+}
+
+const PlanForm: React.FC<PlanProps> = ({nextStep, ...rest}) => {
+
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+
+  const handlePlan = (data: FieldValues) => {
+    console.log('data pla-->', data)
+    nextStep()
+  } 
+
+  return (
+    <>
+      <ContainerForm onSubmit={handleSubmit(handlePlan)}>
+        <Title>Escolha seu plano de assinatura</Title>
+        
+        <Plan 
+          idSelecPlan="radio-1" 
+          preco={60} 
+          title="Lançamento: Coleção completa (oferta limitada)"
+          itensList={['Receba todas as caixas de uma só vez!',
+          'Parcele em até 8x sem juros no cartão de crédito']}
+          {...register('plan')}
+        />                  
+            
+        <Plan 
+          idSelecPlan="radio-2"
+          preco={1500} 
+          title="Plano Anual"
+          itensList={['Parcele em até 12x sem juros no cartão de crédito.',
+          'Economize R$ 127,00.']}
+          {...register('plan')}
+        />
+
+        {formState.errors.plan && <MessageErro> {formState.errors.plan?.message} </MessageErro>}
+        
+        <Buttons>     
+          <FilledButton> Próximo  </FilledButton>
+        </Buttons>
+       
+      </ContainerForm>
+    </>
+  )
+}
+
+
+
+export default PlanForm
---
+++ src/components/stepsForm/planForm/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ContainerForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  gap: 24px;
+`;
+
+export const MessageErro = styled.p`
+  color: #ff0030;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  align-self: flex-end;
+`;
