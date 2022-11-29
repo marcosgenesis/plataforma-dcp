c770c30f3565bd970d32798b03c046477f03f4de
Jeferson Gonçalves <jeferson.goncalves.noronha@gmail.com>
2022-11-29 08:11:28 -0300

    feat: creating buttons for payment types

--- src/components/stepsForm/payForm/index.tsx
+++ src/components/stepsForm/payForm/index.tsx
@@ -9,7 +9,7 @@ import Title from "../../title";
 import { FilledButton } from "../../buttons/FilledButton"
 import { LineButtonForm } from "../../buttons/LineButtonForm"
 
-import {ContainerForm, Buttons, InLine} from './style'
+import {ContainerForm, Buttons, InLine, PayActive, PayActiveContainer} from './style'
 import Input from "../../input";
 import Select from "../../select";
 
@@ -28,6 +28,7 @@ const PayForm: React.FC<PayFormProps> = ({
     resolver: yupResolver(schema),
   });
 
+  const [payActive, setPayActive] = useState(1)
   const handlePay = (data: FieldValues) => {
     console.log('data pay-->', data)
 
@@ -37,55 +38,80 @@ const PayForm: React.FC<PayFormProps> = ({
   return (
     <ContainerForm onSubmit={handleSubmit(handlePay)}>
       <Title>Defina a forma de pagamento</Title>
+      
+      <PayActiveContainer>
+        <PayActive 
+          isCurrent={ payActive === 1 }
+          onClick={() => setPayActive(1)}
+        >
+          Cartão de crédito
+        </PayActive>
+        <PayActive 
+          isCurrent={ payActive === 2 }
+          onClick={() => setPayActive(2)}
+        >
+          Boleto
+        </PayActive>
+      </PayActiveContainer>
+      {payActive === 1 && (
+        <>
+          <InLine>
+            <Input
+              name='numeroCartao'
+              label='Número do cartão'
+              {...register("numeroCartao")}
+              error={formState.errors.numeroCartao}
+              color={"rgba(0, 0, 0, 0.66)"}
+            />
+          </InLine>
 
-      <InLine>
-        <Input
-          name='numeroCartao'
-          label='Número do cartão'
-          {...register("numeroCartao")}
-          error={formState.errors.numeroCartao}
-          color={"rgba(0, 0, 0, 0.66)"}
-        />
-      </InLine>
+          <InLine>
+          
+            <Input
+              name='cvv'
+              label='CVV'
+              {...register("cvv")}
+              error={formState.errors.cvv}
+              color={"rgba(0, 0, 0, 0.66)"}
+            />
+          
+          <Input
+              name='vencimento'
+              label='Vencimento (MM/AA)'
+              {...register("vencimento")}
+              error={formState.errors.vencimento}
+              color={"rgba(0, 0, 0, 0.66)"}
+            />
 
-      <InLine>
-       
-        <Input
-          name='cvv'
-          label='CVV'
-          {...register("cvv")}
-          error={formState.errors.cvv}
-          color={"rgba(0, 0, 0, 0.66)"}
-        />
-       
-       <Input
-          name='vencimento'
-          label='Vencimento (MM/AA)'
-          {...register("vencimento")}
-          error={formState.errors.vencimento}
-          color={"rgba(0, 0, 0, 0.66)"}
-        />
+            <Select
+              name='parcelas'
+              options={[{label: '1 x R$ 600,00', value: 600}, {label:'2 x R$ 300,00', value:300}]}
+              label='Parcelas'
+              error={formState.errors.parcela}
+              {...register("parcela")}
+              color={"rgba(0, 0, 0, 0.66)"}
+            />
+          </InLine>
 
-        <Select
-          name='parcelas'
-          options={[{label: '1 x R$ 600,00', value: 600}, {label:'2 x R$ 300,00', value:300}]}
-          label='Parcelas'
-          error={formState.errors.parcela}
-          {...register("parcela")}
-          color={"rgba(0, 0, 0, 0.66)"}
-        />
-      </InLine>
+          <InLine>
+            <Input
+              name='titular'
+              label='Nome do titular do cartão'
+              {...register("titular")}
+              color={"rgba(0, 0, 0, 0.66)"}
+              error={formState.errors.titular}
+            />
+          
+          </InLine>
+        </>
+      )}
 
-      <InLine>
-        <Input
-          name='titular'
-          label='Nome do titular do cartão'
-          {...register("titular")}
-          color={"rgba(0, 0, 0, 0.66)"}
-          error={formState.errors.titular}
-        />
-       
-      </InLine>
+
+      {payActive === 2 && (
+        <>
+        </>
+      )}
+      
 
       <Buttons>
         <LineButtonForm  type="button" width="110px" onClick={backStep}> Voltar  </LineButtonForm>
--- src/components/stepsForm/payForm/style.ts
+++ src/components/stepsForm/payForm/style.ts
@@ -1,11 +1,40 @@
 import styled from "styled-components";
 
+interface PayActiveProps {
+  isCurrent: boolean;
+}
+
 export const ContainerForm = styled.form`
   display: flex;
   flex-direction: column;
   width: 100%;
 
   gap: 24px;
+`;
+
+export const PayActiveContainer = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 2px;
+
+  height: 40px;
+
+  background: rgba(0, 0, 0, 0.06);
+  border-radius: 8px;
+`;
+
+export const PayActive = styled.div<PayActiveProps>`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  background: ${(props) =>
+    props.isCurrent ? "#FFFFFF" : "rgba(0, 0, 0, 0.06)"};
+  color: ${(props) => (props.isCurrent ? "#0055ff" : "#000000")};
+  font-weight: ${(props) => (props.isCurrent ? "700" : "400")};
+  border-radius: 6px;
 `;
 
 export const InLine = styled.div`
