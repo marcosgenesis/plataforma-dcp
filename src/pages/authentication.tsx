6ce06e1ff499021aa5c6770be2298ee41157548a
Jeferson Gonçalves <jeferson.goncalves.noronha@gmail.com>
2022-12-07 04:26:28 -0300

    fix: passando as informações dos inputs

--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+import { useRouter } from 'next/router'
+import { FieldValues, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import { FilledButton } from "../components/buttons/FilledButton";
 import { LineButton } from "../components/buttons/LineButton";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 import { Container } from "../styles/landingPage";
+import { ToastContainer, toast } from 'react-toastify';
 import {
   AuthFormContainer,
   InputContainer,
@@ -12,12 +17,43 @@ import {
 } from "../styles/authPage";
 import Input from "../components/input";
 import { SocialButton } from "../components/buttons/SocialButton";
+import api from "../services/api";
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const Auth: NextPage = () => {
+  const schema = yup.object().shape({
+  })
+
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const router = useRouter()
+  
+  const handleLogin = (data: FieldValues) => {
+    const { email, password } = data;
+    console.log('-->', email, password)
+    
+    api.post('api/v1/auth', {email, password}).then(() => {
+      router.push('/signature')
+    }).catch(()=>{
+      toast.error('Email/Senha incorreto')
+    })
+
+    
+    // try {
+    //   await login({ email, password, whitelabelId });
+    //   // navigate('/beneficios');
+    // } catch (err) {
+    //   toast.error('Email/Senha incorreto');
+    // }
+  };
+
   return (
     <Container>
       <Navbar />
-      <AuthFormContainer>
+      <AuthFormContainer onSubmit={handleSubmit(handleLogin)}>
         <div>
           <div className="title-subtitle">
             <h1>Que bom te ver de novo!</h1>
@@ -30,8 +66,14 @@ const Auth: NextPage = () => {
             type="email"
             placeholder="exemplo@gmail.com"
             required
+            {...register("email")}
           ></Input>
-          <Input label="Senha" type="password" required></Input>
+          <Input 
+            label="Senha" 
+            type="password" 
+            required 
+            {...register("password")}></Input>
+          
           <div>
             <div className="user-check">
               <div className="check">
@@ -55,8 +97,9 @@ const Auth: NextPage = () => {
           <a href="#">Assine</a>
           <p>um dos nossos planos!</p>
         </div>
-      </AuthFormContainer>
+      </AuthFormContainer>  
       <Footer expanded />
+      <ToastContainer />
     </Container>
   );
 };
