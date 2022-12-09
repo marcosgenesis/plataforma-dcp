78493fe9f591c7212a17bd6187eb9ad2e2cdd4d5
Jeferson Gonçalves <jeferson.goncalves.noronha@gmail.com>
2022-12-09 07:32:25 -0300

    feat: tela de sucesso

---
+++ src/pages/request-success.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Image from "next/image";
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
+import { Content, Container, Header, DeliveryDate, Info, Body } from '../styles/requestSucess'
+import { useDeliveryStore } from "../stores/delivery";
+
+const RequestSucess: React.FC = () => {
+  const { deliveryTime } = useDeliveryStore(({ deliveryTime }) => ({ deliveryTime }));
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Content>
+          <Header>
+            <Image src="/icons/CircleWavyCheck.svg" alt="" width={48} height={48} />
+            <h2>Seu pedido foi realizado com sucesso!</h2>
+          </Header>
+          <Body>
+            <DeliveryDate>
+              <p>PREVISÃO DE ENTREGA</p>
+              <span>DD/MM/AAAA</span>
+              {/* <span> {deliveryTime?.toUpperCase()} </span> */}
+            </DeliveryDate>
+            <Info>
+              <div>
+                <Image src='/icons/exclamacao.svg' alt="" width={16} height={16}/>
+                <li>
+                  Você pode acompanhar o status do pedido pelo seu perfil ou pelo email cadastrado na hora da compra.
+                </li>
+              </div>
+
+              <div>
+                <Image src='/icons/exclamacao.svg' alt="" width={16} height={16}/>
+                <li>
+                  A previsão de entrega pode sofrer alterações de acordo com a confirmação de pagamento.
+                </li>
+              </div>
+            </Info>
+          </Body>
+        </Content>
+      </Container>
+      <Footer expanded={false} />
+    </>
+  );
+};
+
+export default RequestSucess;
